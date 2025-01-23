b7e533e16f8151be9675a288ad40f9ced3bcd52d
Gatjuat-Wicteat-Riek <gatjuatriek@gmail.com>
2025-01-23 05:52:40 +0200

    Adding swipers

--- frontend/src/components/featuredProperties/FeaturedProp.jsx
+++ frontend/src/components/featuredProperties/FeaturedProp.jsx
@@ -1,10 +1,6 @@
 import "./featuredProp.css";
-import useFetch from "../../hooks/useFetch.js";
 
 const FeaturedProp = () => {
-  const { data, loading, error } = useFetch("hotels/countByCity?cities=berlin,madrid,london")
-  console.log(data)
-
   return (
     <div className="fp">
       <div className="fpItem">
