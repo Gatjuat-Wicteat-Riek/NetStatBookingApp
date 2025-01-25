0eca3a986a9ddc43fa29865ea75a13073f42c5c3
Gatjuat-Wicteat-Riek <gatjuatriek@gmail.com>
2025-01-25 04:49:57 +0200

    Adding Logout

--- frontend/src/components/navbar/NavBar.jsx
+++ frontend/src/components/navbar/NavBar.jsx
@@ -1,27 +1,44 @@
 import "./Navbar.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";  ory
 import { useContext } from "react";
-import {AuthContext} from "../context/AuthContext.jsx";
+import { AuthContext } from "../context/AuthContext.jsx";
+
 const Navbar = () => {
-    const { user } = useContext(AuthContext);
+    const { user, dispatch } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    // Logout handler function
+    const handleLogout = () => {
+        dispatch({ type: "LOGOUT" });
+        navigate("/");
+    };
 
     return (
         <div className="navbarItem">
             <div className="navContainer">
-                <Link to="/" style={{color: "inherit", textDecoration: "none"}}>
-                    <span className="TLogo">
-                      <span className="FirstLetter">Chan</span>
-                      <span className="logoSpan">Yong</span>
-                      <span className="LogoEnd">Booking</span>
-                     </span>
+                <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+          <span className="TLogo">
+            <span className="FirstLetter">Chan</span>
+            <span className="logoSpan">Yong</span>
+            <span className="LogoEnd">Booking</span>
+          </span>
                 </Link>
-                {user ? user.username : (
+
+                {user ? (
+                    <div className="navItems">
+            <span style={{ color: '#8338ec', marginRight: '10px' }}>
+              {user.username}
+            </span>
+                        <button className="navButton" onClick={handleLogout}>
+                            Logout
+                        </button>
+                    </div>
+                ) : (
                     <div className="navItems">
                         <button className="navButton">Register</button>
                         <Link to="/login">
-                        <button className="navButton">Login</button>
+                            <button className="navButton">Login</button>
                         </Link>
-
                     </div>
                 )}
             </div>
